647a10205c95875f473acc2178a6c4b3efe596cf
Nad Lambino <ronaldlambino0000@gmail.com>
2022-09-03 17:03:05 +0800

    Rebuild

--- dist/v-directives.es.js
+++ dist/v-directives.es.js
@@ -1,1 +1,10 @@
-
+const s = {
+  mounted(o) {
+    o.focus();
+  }
+}, t = {
+  focus: s
+};
+export {
+  t as default
+};
--- dist/v-directives.umd.js
+++ dist/v-directives.umd.js
@@ -1,1 +1,1 @@
-(function(n){typeof define=="function"&&define.amd?define(n):n()})(function(){"use strict"});
+(function(e,n){typeof exports=="object"&&typeof module<"u"?module.exports=n():typeof define=="function"&&define.amd?define(n):(e=typeof globalThis<"u"?globalThis:e||self,e["v-directives"]=n())})(this,function(){"use strict";return{focus:{mounted(i){i.focus()}}}});
